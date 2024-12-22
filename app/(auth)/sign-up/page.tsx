93364243c9d530c0da43cf1214f23ed3457e2f0a
masterthuku <peterthuku706@gmail.com>
2024-12-22 16:51:37 +0300

    auth UI

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
+import AuthForm from "@/components/AuthForm";
 
-const SignUp = () => {
-  return <div>sign-up</div>;
-};
+const SignUp = () => <AuthForm type="sign-up" />;
 
 export default SignUp;
